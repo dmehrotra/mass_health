326f3a236b03fd228740e4e4eea7d5b2edef6eab
Carl Schwope <schwope.carl@gmail.com>
2013-09-04 12:21:10 -0400

    working on column selector

--- app/controllers/town_health_record_controller.rb
+++ app/controllers/town_health_record_controller.rb
@@ -1,11 +1,15 @@
 class TownHealthRecordController < ApplicationController
 
+  include TownHealthRecordHelper
+
   def index
 
-    if params[:town] &&
-       params[:town].length > 0
+    choice = get_choice( params[:column_select] )
 
-      @records = TownHealthRecord.where( town: params[:town] )
+    if params[:column] &&
+       params[:column].length > 0
+
+      @records = TownHealthRecord.where( choice.to_sym params[:column] )
     else
       @records = TownHealthRecord.limit(params[:limit].to_i + 2)
     end
