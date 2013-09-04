abb8dac3031237c85673705c36a254b11bb83437
Carl Schwope <schwope.carl@gmail.com>
2013-09-04 11:41:19 -0400

    refactor text fields

--- app/controllers/town_health_record_controller.rb
+++ app/controllers/town_health_record_controller.rb
@@ -2,13 +2,12 @@ class TownHealthRecordController < ApplicationController
   include TownHealthRecordHelper
   def index
 
-    if params[:town_health_record] &&
-       params[:town_health_record][:town] &&
-       params[:town_health_record][:town].length > 0
+    if params[:town] &&
+       params[:town].length > 0
 
-      @records = TownHealthRecord.where( town: params[:town_health_record][:town] ).limit(get_limit)
+      @records = TownHealthRecord.where( town: params[:town] )
     else
-      @records = TownHealthRecord.all.limit(get_limit)
+      @records = TownHealthRecord.limit(params[:limit].to_i + 2)
     end
   end
 
