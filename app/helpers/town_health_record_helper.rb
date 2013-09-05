151fb1c375ed4e346dfac20ffc8d3db2efd1c35a
Carl Schwope <schwope.carl@gmail.com>
2013-09-04 21:39:35 -0400

    column options method

--- app/helpers/town_health_record_helper.rb
+++ app/helpers/town_health_record_helper.rb
@@ -1,27 +1,30 @@
 module TownHealthRecordHelper
 
   def table_headers
-    titles.map { | title | "<th>#{title}</th>" }.join.html_safe
+    titles.map { | pair | "<th>#{pair[1]}</th>" }.join.html_safe
   end
 
+  def column_options
+    titles.map { | pair | "<option value='#{pair[0]}'>#{pair[1]}</option>" }.join.html_safe
+  end
 
   def titles
     [
-      "Geography",
-      "total pop, year 2005",
-      "age 0-19, year 2005",
-      "age 65+, year 2005",
-      "Per Capita Income, year 2000",
-      "Persons Living Below 200% Poverty, year 2000",
-      "% all Persons Living Below 200% Poverty Level, year 2000",
-      "% adequacy prenatal care (kotelchuck)",
-      "% C-section deliveries, 2005-2008",
-      "Number of infant deaths, 2005-2008",
-      "Infant mortality rate (deaths per 1000 live births), 2005-2008",
-      "% low birthweight 2005-2008",
-      "% multiple births, 2005-2008",
-      "% publicly financed prenatal care, 2005-2008",
-      "% teen births, 2005-2008"
+      ['town',"Geography"],
+      ['total_population',"total pop, year 2005"],
+      ['youth_population',"age 0-19, year 2005"],
+      ['elder_population',"age 65+, year 2005"],
+      ['income',"Per Capita Income, year 2000"],
+      ['poverty',"Persons Living Below 200% Poverty, year 2000"],
+      ['poverty_percentage',"% all Persons Living Below 200% Poverty Level, year 2000"],
+      ['prenatal_care',"% adequacy prenatal care (kotelchuck)"],
+      ['c_section',"% C-section deliveries, 2005-2008"],
+      ['infant_deaths',"Number of infant deaths, 2005-2008"],
+      ['infant_mortality',"Infant mortality rate (deaths per 1000 live births), 2005-2008"],
+      ['low_birthweight',"% low birthweight 2005-2008"],
+      ['multiple_births',"% multiple births, 2005-2008"],
+      ['publicly_financed_prenatal',"% publicly financed prenatal care, 2005-2008"],
+      ['teen_preg',"% teen births, 2005-2008"]
     ]
   end
 end
